a144b48c2d2cc99b73e45c4175235a61392c09b0
dantonsoftware <danton.cortes@gmail.com>
2025-09-23 21:03:07 -0300

    Add Db Seeder

---
+++ Orders/Orders.Backend/Data/SeedDb.cs
@@ -0,0 +1,40 @@
+﻿using Orders.Shared.Entities;
+
+namespace Orders.Backend.Data;
+
+public class SeedDb
+{
+    private readonly DataContext _context;
+
+    public SeedDb(DataContext context)
+    {
+        this._context = context;
+    }
+
+    public async Task SeedAsync()
+    {
+        await _context.Database.EnsureCreatedAsync();
+        await CheckCategoriesAsync();
+        await CheckCountriesAsync();
+    }
+
+    private async Task CheckCategoriesAsync()
+    {
+        if (!_context.Categories.Any())
+        {
+            _context.Categories.Add(new Category { Name = "Calzado" });
+            _context.Categories.Add(new Category { Name = "Tecnologia" });
+            await _context.SaveChangesAsync();
+        }
+    }
+
+    private async Task CheckCountriesAsync()
+    {
+        if (!_context.Categories.Any())
+        {
+            _context.Countries.Add(new Country { Name = "Colombia" });
+            _context.Countries.Add(new Country { Name = "Chile" });
+            await _context.SaveChangesAsync();
+        }
+    }
+}
