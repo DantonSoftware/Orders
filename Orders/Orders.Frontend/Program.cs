b8951771cf2453abbc3c4470b591e2f029032334
dantonsoftware <danton.cortes@gmail.com>
2025-10-12 20:12:37 -0300

    Implementando Front Blazor

--- Orders/Orders.Frontend/Program.cs
+++ Orders/Orders.Frontend/Program.cs
@@ -1,5 +1,6 @@
-using MudBlazor.Services;
+﻿using MudBlazor.Services;
 using Orders.Frontend.Components;
+using Orders.Frontend.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,22 +11,28 @@ builder.Services.AddMudServices();
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri("https://localhost:7262") });
+builder.Services.AddScoped<IRepository, Repository>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();        // ← añade esto
 
-
-app.UseAntiforgery();
+// 🟩 ORDEN CORRECTO: después de UseStaticFiles y antes de MapRazorComponents
+app.UseRouting();
+app.UseAntiforgery(); // ← aquí va, entre UseRouting y MapRazorComponents
 
 app.MapStaticAssets();
+
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
